f86272a42a9b0dae920462f0439f0ce6463cf74c
Paras Ghughtyal <antique2024@gmail.com>
2025-06-15 14:43:13 +0530

    Add solved problems batch

---
+++ LeetCode/Hashmaps/TwoSum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    int n = nums.size();
+    unordered_map<int,int> map(n);
+    
+    for(int i=0; i<n; i++ ){
+
+        if(map.find(target - nums[i]) != map.end())
+        return {map[target - nums[i]], i};
+
+        else
+        map[nums[i]] = i;
+    }
+
+    return {};
+  }
+};
