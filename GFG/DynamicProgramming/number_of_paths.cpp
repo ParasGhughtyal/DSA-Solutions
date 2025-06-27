86cc352ff7c6d05055ea711d0b8418b3278bc7dc
Paras Ghughtyal <antique2024@gmail.com>
2025-06-27 21:53:10 +0530

    Added Solution : Number of paths (Medium)

---
+++ GFG/DynamicProgramming/number_of_paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+  
+    int path(int row, int col, vector<vector<int>>& dp)
+    {
+        if(row == 0 && col == 0)
+        return 1;
+        
+        if(row < 0 || col < 0)
+        return 0;
+        
+        if(dp[row][col] != -1)
+        return dp[row][col];
+        
+        return dp[row][col] = path(row-1, col, dp) + path(row, col-1, dp);
+        
+    }
+    
+    int numberOfPaths(int m, int n) {
+        // Code Here
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        
+        return path(m-1, n-1, dp);
+    }
+};
+
