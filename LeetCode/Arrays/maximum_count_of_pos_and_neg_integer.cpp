3bf24f6f2701ab117f276b7aad6ae59c29a3cf4b
Paras Ghughtyal <antique2024@gmail.com>
2025-06-30 22:44:52 +0530

    Added Solution : Maximum count of positive integer and negative integer (Easy)

---
+++ LeetCode/Arrays/maximum_count_of_pos_and_neg_integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        
+        int neg_count = 0, pos_count = 0;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i] > 0)
+            pos_count++;
+
+            else if(nums[i] < 0)
+            neg_count++;
+        }
+
+        return max(pos_count, neg_count);
+    }
+};
