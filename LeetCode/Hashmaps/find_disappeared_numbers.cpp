5cf90e03a129dc934288937547015b493d374d20
Paras Ghughtyal <antique2024@gmail.com>
2025-07-06 19:03:52 +0530

    Added Solution : Find all numbers disappeared in an array (Easy)

---
+++ LeetCode/Hashmaps/find_disappeared_numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+        unordered_map<int, bool> seen;
+
+        for(int &num : nums)
+        seen[num] = true;
+        
+
+        vector<int> result;
+
+        for(int i = 1; i <= nums.size(); i++)
+        {
+            if(!seen[i]) 
+            result.push_back(i);
+            
+        }
+
+        return result;
+    }
+};
